785c4b97f560c76e66bb74d046969e9eeed99f1a
Žiga Lah <ziga.lah1@student.um.si>
2020-03-27 14:40:46 +0100

    Changed navigation and added new pages.

--- header.php
+++ header.php
@@ -34,7 +34,7 @@
                 <a class="nav-link" href="index.php/intervencije">Intervencije</a>
             </li>
             <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                <a class="nav-link dropdown-toggle" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                 Informacije
                 </a>
                 <div class="dropdown-menu" aria-labelledby="navbarDropdown">
