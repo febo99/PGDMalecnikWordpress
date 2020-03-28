235ad71f08bdd27e43a80c5714496fd3373cd601
Žiga Lah <ziga.lah1@student.um.si>
2020-03-28 12:33:28 +0100

    Added functions for text.

--- page-o-nas.php
+++ page-o-nas.php
@@ -3,21 +3,16 @@
         <img src='<?php echo get_theme_mod('pgdAboutUsThumbnailCalloutSectionImage');?>'>
     </div>
     <div class='description'>
-        <h1>O nas</h1>
+        <h1><?php echo get_theme_mod('pgdHistoryThumbnailCalloutSectionTitle'); ?></h1>
         <p>
-        Prostovoljna gasilska enota 2. kategorije 2. sektor pri JGS Maribor - GZ Maribor. 
+        <?php echo get_theme_mod('pgdAboutUsThumbnailCalloutSectionP'); ?>
         </p>
     </div>
     <div class='textInfo'>
         <div class='container'>
-            <h1>O PGD Malečnik</h1>
+            <h1><?php echo get_theme_mod('pgdAboutUsThumbnailCalloutSectionONasHeader'); ?></h1>
             <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum sem dui, varius consequat massa in, eleifend viverra ante. Morbi id odio a justo dictum finibus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nulla faucibus vel elit vitae pellentesque. 
-            Cras non felis quis nibh placerat auctor at a tortor. Morbi congue, diam nec elementum congue, magna nunc blandit elit, eget semper nibh ante non sapien. Vestibulum libero tellus, faucibus eu nulla sed, euismod malesuada nibh. Donec enim arcu, commodo sed mi ac, interdum pellentesque ex.
-             Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; 
-            Quisque a interdum neque, in convallis nunc. Nulla magna lacus, fringilla non ante vitae, elementum fringilla ligula. Praesent sem justo, viverra in sapien vel, venenatis consectetur augue. Curabitur accumsan commodo lorem. Nulla viverra massa nisi, vel maximus massa auctor nec. Morbi tristique porta maximus.
-            Duis a metus tempor, iaculis velit a, sodales tortor. Quisque vel lacus in justo tristique eleifend. Etiam at lorem at sem lobortis pretium eget sed nulla. Aliquam blandit neque sit amet nibh varius, at dictum augue aliquam. Etiam ac malesuada nisl, id pellentesque orci. 
-            Nunc convallis nisl non elit vestibulum faucibus. Proin pulvinar ex risus. 
+            <?php echo get_theme_mod('pgdAboutUsThumbnailCalloutSectionONasOpis'); ?>
         </p>
         </div>
     </div>
@@ -26,14 +21,9 @@
     </div>
     <div class='pioneersInfo'>
         <div class='container'>
-            <h1>Pionirke in pionirji </h1>
+            <h1><?php echo get_theme_mod('pgdAboutUsThumbnailCalloutSectionPioneersHeader'); ?></h1>
             <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum sem dui, varius consequat massa in, eleifend viverra ante. Morbi id odio a justo dictum finibus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nulla faucibus vel elit vitae pellentesque. 
-            Cras non felis quis nibh placerat auctor at a tortor. Morbi congue, diam nec elementum congue, magna nunc blandit elit, eget semper nibh ante non sapien. Vestibulum libero tellus, faucibus eu nulla sed, euismod malesuada nibh. Donec enim arcu, commodo sed mi ac, interdum pellentesque ex.
-             Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; 
-            Quisque a interdum neque, in convallis nunc. Nulla magna lacus, fringilla non ante vitae, elementum fringilla ligula. Praesent sem justo, viverra in sapien vel, venenatis consectetur augue. Curabitur accumsan commodo lorem. Nulla viverra massa nisi, vel maximus massa auctor nec. Morbi tristique porta maximus.
-            Duis a metus tempor, iaculis velit a, sodales tortor. Quisque vel lacus in justo tristique eleifend. Etiam at lorem at sem lobortis pretium eget sed nulla. Aliquam blandit neque sit amet nibh varius, at dictum augue aliquam. Etiam ac malesuada nisl, id pellentesque orci. 
-            Nunc convallis nisl non elit vestibulum faucibus. Proin pulvinar ex risus. 
+            <?php echo get_theme_mod('pgdAboutUsThumbnailCalloutSectionPioneersP'); ?>
             </p>
             <div class='imageGallery'>
                 <div class='row'>
@@ -55,14 +45,9 @@
     </div>
     <div class='juniorsInfo'>
         <div class='container'>
-            <h1>Mladinke in mladinci</h1>
+            <h1><?php echo get_theme_mod('pgdAboutUsThumbnailCalloutSectionJuniorsHeader'); ?></h1>
             <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum sem dui, varius consequat massa in, eleifend viverra ante. Morbi id odio a justo dictum finibus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nulla faucibus vel elit vitae pellentesque. 
-            Cras non felis quis nibh placerat auctor at a tortor. Morbi congue, diam nec elementum congue, magna nunc blandit elit, eget semper nibh ante non sapien. Vestibulum libero tellus, faucibus eu nulla sed, euismod malesuada nibh. Donec enim arcu, commodo sed mi ac, interdum pellentesque ex.
-             Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; 
-            Quisque a interdum neque, in convallis nunc. Nulla magna lacus, fringilla non ante vitae, elementum fringilla ligula. Praesent sem justo, viverra in sapien vel, venenatis consectetur augue. Curabitur accumsan commodo lorem. Nulla viverra massa nisi, vel maximus massa auctor nec. Morbi tristique porta maximus.
-            Duis a metus tempor, iaculis velit a, sodales tortor. Quisque vel lacus in justo tristique eleifend. Etiam at lorem at sem lobortis pretium eget sed nulla. Aliquam blandit neque sit amet nibh varius, at dictum augue aliquam. Etiam ac malesuada nisl, id pellentesque orci. 
-            Nunc convallis nisl non elit vestibulum faucibus. Proin pulvinar ex risus. 
+            <?php echo get_theme_mod('pgdAboutUsThumbnailCalloutSectionJuniorsP'); ?>
             </p>
             <div class='3imageGallery'>
                 <div class='row'>
@@ -84,14 +69,9 @@
     </div>
     <div class='veteransInfo'>
         <div class='container'>
-            <h1>Veterani</h1>
+            <h1><?php echo get_theme_mod('pgdAboutUsThumbnailCalloutSectionVeteransHeader'); ?></h1>
             <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum sem dui, varius consequat massa in, eleifend viverra ante. Morbi id odio a justo dictum finibus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nulla faucibus vel elit vitae pellentesque. 
-            Cras non felis quis nibh placerat auctor at a tortor. Morbi congue, diam nec elementum congue, magna nunc blandit elit, eget semper nibh ante non sapien. Vestibulum libero tellus, faucibus eu nulla sed, euismod malesuada nibh. Donec enim arcu, commodo sed mi ac, interdum pellentesque ex.
-             Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; 
-            Quisque a interdum neque, in convallis nunc. Nulla magna lacus, fringilla non ante vitae, elementum fringilla ligula. Praesent sem justo, viverra in sapien vel, venenatis consectetur augue. Curabitur accumsan commodo lorem. Nulla viverra massa nisi, vel maximus massa auctor nec. Morbi tristique porta maximus.
-            Duis a metus tempor, iaculis velit a, sodales tortor. Quisque vel lacus in justo tristique eleifend. Etiam at lorem at sem lobortis pretium eget sed nulla. Aliquam blandit neque sit amet nibh varius, at dictum augue aliquam. Etiam ac malesuada nisl, id pellentesque orci. 
-            Nunc convallis nisl non elit vestibulum faucibus. Proin pulvinar ex risus. 
+            <?php echo get_theme_mod('pgdAboutUsThumbnailCalloutSectionVeteransP'); ?>
             </p>
             <div class='3imageGallery'>
                 <div class='row'>
@@ -113,14 +93,9 @@
     </div>
     <div class='operativeInfo'>
         <div class='container'>
-            <h1>Operativna enota</h1>
+            <h1><?php echo get_theme_mod('pgdAboutUsThumbnailCalloutSectionOperativesHeader'); ?></h1>
             <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum sem dui, varius consequat massa in, eleifend viverra ante. Morbi id odio a justo dictum finibus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nulla faucibus vel elit vitae pellentesque. 
-            Cras non felis quis nibh placerat auctor at a tortor. Morbi congue, diam nec elementum congue, magna nunc blandit elit, eget semper nibh ante non sapien. Vestibulum libero tellus, faucibus eu nulla sed, euismod malesuada nibh. Donec enim arcu, commodo sed mi ac, interdum pellentesque ex.
-             Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; 
-            Quisque a interdum neque, in convallis nunc. Nulla magna lacus, fringilla non ante vitae, elementum fringilla ligula. Praesent sem justo, viverra in sapien vel, venenatis consectetur augue. Curabitur accumsan commodo lorem. Nulla viverra massa nisi, vel maximus massa auctor nec. Morbi tristique porta maximus.
-            Duis a metus tempor, iaculis velit a, sodales tortor. Quisque vel lacus in justo tristique eleifend. Etiam at lorem at sem lobortis pretium eget sed nulla. Aliquam blandit neque sit amet nibh varius, at dictum augue aliquam. Etiam ac malesuada nisl, id pellentesque orci. 
-            Nunc convallis nisl non elit vestibulum faucibus. Proin pulvinar ex risus. 
+            <?php echo get_theme_mod('pgdAboutUsThumbnailCalloutSectionOperativesP'); ?>
             </p>
             <div class='3imageGallery'>
                 <div class='row'>
