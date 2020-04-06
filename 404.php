424349e1896626164e727593672d3426e8973068
Žiga Lah <ziga.lah1@student.um.si>
2020-04-06 19:44:46 +0200

    Fixed some css errors.

--- 404.php
+++ 404.php
@@ -1,5 +1,5 @@
 <?php get_header();?>
-    <div class='container'>
+    <div class='container container404'>
         <img class='icon404' src="<?php echo get_template_directory_uri() ?>/images/404Icon.png" >
         <h1>Ta stran ne obstaja!</h1>
         <p>Niste našli kar ste iskali? Vrnite se na <a href='/PGDMalecnik'>domačo stran</a>.
